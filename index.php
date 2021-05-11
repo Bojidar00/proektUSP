dd98e42eb73e016351600beb291e2c897549a1d4
Bojidar00 <80902025+Bojidar00@users.noreply.github.com>
2021-05-11 14:36:45 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -41,7 +41,7 @@ div {
         
         
         <div>
-<form>
+<form action=# method="POST">
     Марка:
     <select id="marka" name="marka" onchange="change()" style="margin:10px 0px" ><br>
                            <option >--изберете--</option><br>
@@ -55,10 +55,56 @@ div {
     <select name="model" id=model>
     <option >--изберете--</option>
     </select><br>
+     Екстри:<br>
+    <?php
+                                include "config.php";
+                                 $result =mysqli_query($dbConn, "SELECT id_ekstra,ekstra FROM ekstri");
+                                while($row = mysqli_fetch_array($result)){
+                                 echo '<input type="checkbox"  style="width:10%" name="ekstri[]"value="'.$row['id_ekstra'].'">
+                                 <label for="'.$row['id_ekstra'].'">'.$row['ekstra'].'</label>';}
+                                 ?>
     <input type="submit" name="submit" value="Търси">
 </form>
 </div>
 </center>
+                   <?php 
+                     include "config.php";
+                     if (isset($_POST["submit"])){
+                      if (isset($_POST["marka"])&&isset($_POST["model"])&&isset($_POST["ekstri"])){
+                        $marka=$_POST['marka'];
+                        $model=$_POST['model'];
+                        $ekstri=$_POST['ekstri'];
+                           echo $marka,$model,$ekstri[0];
+                        $ekst=" ";
+                        for ($x = 0; $x < sizeof($ekstri); $x++){
+                          $u=$ekstri[$x];
+                               
+                              $ekst=$ekst." AND koli_ekstri.id_ekstra=".$u;
+                           
+                            }
+
+                               
+                        $sql="Select koli.id_kola,snimka,marka,model from koli 
+                        join model on model.id_model=koli.id_model
+                        join marka on marka.id_marka=model.id_marka 
+                        join koli_ekstri on koli_ekstri.id_kola=koli.id_kola
+                        where marka=".$marka." AND model=".$model."".$ekst.";";
+                         
+                        echo $sql;
+                        $call = $dbConn->prepare($sql);
+                        //$call->bind_param("ii",$marka,$model);
+
+                        if (!$call->execute()){echo "Грешка.";}
+                            else {echo "Добавихте един запис.";}
+                    
+
+
+                      }
+
+
+
+                     }
+                     ?>
 
 <?php
  $call  = $dbConn->prepare("SELECT model,id_model,id_marka from model Order by id_marka;");
@@ -104,24 +150,7 @@ div {
       }
       document.getElementById("model").innerHTML = str;
     }
-    //* 
-	function change() {
-      var x =document.getElementById("marka").value;
-     var str=" <option >--изберете--</option>";
-     
-      if(x!="--изберете--"){
-      for (i = 0; i < eval('ar'+x).length; i++) {
-        
-             str+=" <option value='"+eval('ar'+x)[i]+"' >";
-             i++;
-             str+=eval('ar'+x)[i]+"</option>";
-           }
-           
-      }
-      document.getElementById("model").innerHTML = str;
-    }
-	
-    *//
+    
     
         </script>
 
--- vuvedi.php
+++ vuvedi.php
@@ -37,7 +37,7 @@ div {
         <center>
             <h1>Въведи автомобил</h1>
         <div>
-<form>
+<form action=# method="POST" enctype="multipart/form-data">
     Марка:
     <select id="marka" name="marka" onchange="change()" style="margin:10px 0px" ><br>
                            <option >--изберете--</option><br>
@@ -51,10 +51,78 @@ div {
     <select name="model" id=model>
     <option >--изберете--</option>
     </select><br>
+    Екстри:<br>
+    <?php
+                                include "config.php";
+                                 $result =mysqli_query($dbConn, "SELECT id_ekstra,ekstra FROM ekstri");
+                                while($row = mysqli_fetch_array($result)){
+                                 echo '<input type="checkbox"  style="width:10%" name="ekstri[]"value="'.$row['id_ekstra'].'">
+                                 <label for="'.$row['id_ekstra'].'">'.$row['ekstra'].'</label>';}
+                                 ?><br>
+  <input type="file" name="snimka" ><br>
     <input type="submit" name="submit" value="Въведи">
 </form>
 </div>
 </center>
+
+
+<?php
+include "config.php";
+if (isset($_POST["submit"])){
+ if (isset($_POST["marka"])&&isset($_POST["model"])&&isset($_POST["ekstri"])&&isset($_FILES["snimka"]["name"])){
+   $marka=$_POST['marka'];
+   $model=$_POST['model'];
+   $ekstri=$_POST['ekstri'];
+   $snimka=$_FILES["snimka"]["name"];
+      echo $marka,$model,$ekstri[0],$snimka;
+   $ekst=" ";
+   for ($x = 0; $x < sizeof($ekstri); $x++){
+     $u=$ekstri[$x];
+          
+         
+      
+       }
+
+          
+   $sql="Insert into koli(id_model,snimka)
+   Values (".$model.",".'"'.$snimka.'"'.");";
+    echo $sql;
+   $call = $dbConn->prepare($sql);
+  
+
+   if (!$call->execute()){echo "Грешка.";}
+       else {echo "Добавихте един запис.";}
+
+
+
+
+
+       $target_dir = "uploads/";
+       $target_file = $target_dir . basename($_FILES["snimka"]["name"]);
+
+
+       if (move_uploaded_file($_FILES["snimka"]["tmp_name"], $target_file)) {
+        echo "The file ". htmlspecialchars( basename( $_FILES["snimka"]["name"])). " has been uploaded.";
+      } else {
+        echo "Sorry, there was an error uploading your file.";
+      }
+ }
+
+
+
+}
+
+
+
+
+
+
+
+?>
+
+
+
+
 
 <?php
  $call  = $dbConn->prepare("SELECT model,id_model,id_marka from model Order by id_marka;");
